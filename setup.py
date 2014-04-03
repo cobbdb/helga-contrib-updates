f4aee95b84945316b24003fec55ba1eebc6cfe45
Dan <cobbdb@gmail.com>
2014-04-03 08:39:15 -0700

    status 5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
